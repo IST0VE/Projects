fe410315a19cd426cca512f67e118b84ad017340
Nikita Korovko <nikorovko@gmail.com>
2023-02-13 16:42:07 +0400

    lab_2 v2.0

--- Laba_2_KSIT/client.py
+++ Laba_2_KSIT/client.py
@@ -1,19 +1,27 @@
 import socket
-
+# создаем сокет
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# связываем сокет с локальным адресом
 clientsocket.connect(('localhost', 20001))
-
+# вводим имя
 client_name = input("Enter your name: ")
 clientsocket.send(client_name.encode())
 
 while True:
+    # вводим строку
     request = input("Enter your request: ")
+    # если строка равна "Покажи время"
     if request == "Покажи время":
+        # отправляем запрос серверу
         clientsocket.send(request.encode())
+        # получаем ответ от сервера
         response = clientsocket.recv(1024).decode()
+        # выводим ответ
         print(response)
     else:
+        # если строка не равна "Покажи время", выходим из цикла
         print("Closing connection...")
         clientsocket.close()
         break
+# закрываем сокет
 clientsocket.close()
